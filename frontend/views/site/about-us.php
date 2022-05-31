fe4ea1c737a1740761cd62e402f7622714ca4818
Mazen AlSamman <batman@pop-os.localdomain>
2022-05-31 18:40:20 +0300

    Contact Page

--- frontend/views/site/about-us.php
+++ frontend/views/site/about-us.php
@@ -2,6 +2,40 @@
 $this->title = "تواصل معنا";
 ?>
 
-<div class="container text-center">
-    <h1 class="alert alert-warning">بـانتظـار المحتوى</h1>
+<div class="container-fluid text-center">
+    <div class="row content-center mt-5">
+        <div class="col-lg-12 services-section">
+            <h1 class="section-heading centered font-din">تواصل معنا</h1>
+            <div class="row content-center">
+                <div class="col-lg-3 mb-3 p-3 rounded text-center bg-light">
+                    <h1 class="font-din mt-lg-5">هاتف أو واتساب</h1>
+                    <div>
+                        <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="whatsapp"/>
+                        <span class="text-success">&</span>
+                        <img src="https://img.icons8.com/dotty/48/26e07f/callback.png" alt="Call"/>
+                    </div>
+                    <a href="https://iwtsp.com/966505931313" class="font-15 mt-3 d-block"
+                       target="_blank">+966-505931313</a>
+                </div>
+                <div class="col-lg-3 mb-3 ml-3 mr-3 rounded text-center bg-light">
+                    <h1 class="font-din">موقعنا</h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28824.67751926832!2d49.5743!3d25.435432!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xafd20fdf3924c980!2z2KfZhNiz2K_YsdipINin2YTYudmC2KfYsdmK2Kk!5e0!3m2!1sar!2ssa!4v1651273757551!5m2!1sar!2ssa"
+                            height="200" style="border:0; width: 100%" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-lg-3 mb-3 p-3 rounded text-center bg-light">
+                    <h1 class="font-din mt-lg-4">تقييم العقار</h1>
+                    <div>
+                        <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="whatsapp"/>
+                        <span class="text-success">&</span>
+                        <img src="https://img.icons8.com/dotty/48/26e07f/callback.png" alt="Call"/>
+                    </div>
+                    <a href="https://iwtsp.com/966504831313" class="font-15 mt-3 d-block"
+                       target="_blank">+966-504831313</a>
+                    <a href="https://iwtsp.com/966504931313" class="font-15 mt-3 d-block"
+                       target="_blank">+966-504931313</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
